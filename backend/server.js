0dec3e88f90c08f1b66a00e9bbeaee057a095acc
Anurag Tripathi <113921508+anurag24-26@users.noreply.github.com>
2025-07-15 09:51:13 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
 const connectDB = require("./config/db");
 
 const authRoutes = require("./routes/auth");
@@ -11,13 +12,22 @@ connectDB();
 
 const app = express();
 
-app.use(cors({ origin: "*" }));
+// Middleware
+app.use(
+  cors({
+    origin: "*", // ✅ Update with frontend URL
+    credentials: true,               // ✅ Allow sending cookies
+  })
+);
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/bucketlist", bucketRoutes);
 
+// Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`✅ Server running on http://localhost:${PORT}`);
